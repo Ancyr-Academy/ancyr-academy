c6b94ea4acbd9531433bb14a8fc7715c8c3a2a1f
Anthony Cyrille <ancyr@pm.me>
2024-03-12 08:05:14 +0100

    fix: discount

--- apps/web/src/modules/promotion/index.ts
+++ apps/web/src/modules/promotion/index.ts
@@ -1,8 +1,10 @@
+"use client";
+
 export const getSalesDiscount = () => ({
-  startsAt: new Date("2024-03-05T00:00:00.000Z"),
-  endsAt: new Date("2024-03-11T00:00:00.000Z"),
-  percentage: 0.3,
-  code: "CLEAN30",
+  startsAt: new Date("2024-03-12T00:00:00.000Z"),
+  endsAt: new Date("2024-03-14T00:00:00.000Z"),
+  percentage: 0.4,
+  code: "CLEAN40",
 });
 
 export const isDiscountActive = (date: Date = new Date()) => {
