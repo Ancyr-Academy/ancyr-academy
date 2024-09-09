322c9caee7303543c5c1d96a1a521dd23ffb8d0d
Anthony Cyrille <ancyr@pm.me>
2024-09-09 09:27:02 +0200

    fix: pricing

--- apps/web/src/modules/training/Training.tsx
+++ apps/web/src/modules/training/Training.tsx
@@ -41,7 +41,7 @@ export const Training: React.FC<{ training: TrainingType }> = ({
             </KeyPoint>
             <KeyPoint>
               <h3>Tarif</h3>
-              <b>Entreprise / Freelance</b> : 1.200€ HT
+              <b>Entreprise / Freelance</b> : 1.000€ HT
               <br />
               <b>Salarié / Etudiant</b> : 400€ HT
               <br />
