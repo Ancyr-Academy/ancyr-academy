11a00ecc795ecb9697670423e313ec31bca388b6
Anthony Cyrille <ancyr@pm.me>
2024-11-02 06:47:36 +0100

    task: removed mdx

--- apps/web/src/app/blog/[slug]/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { Container } from "../../../ui/Container";
-
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <Container>{children}</Container>;
-};
-
-export default Layout;
