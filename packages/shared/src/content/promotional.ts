714a841a979140e658d83b5bdebb0f200eb51e06
Anthony Cyrille <ancyr@pm.me>
2025-02-22 08:12:56 +0100

    task: discount

--- packages/shared/src/content/promotional.ts
+++ packages/shared/src/content/promotional.ts
@@ -9,25 +9,29 @@ const totalSlots = 8;
 const discountStates = [
   {
     percentage: 0.1,
-    code: "CLEAN10",
+    code: "CODE10",
     slots: [0, 1],
   },
   {
-    percentage: 0.2,
-    code: "CLEAN20",
+    percentage: 0.15,
+    code: "CODE15",
     slots: [3, 4],
   },
   {
     percentage: 0.1,
-    code: "CLEAN10",
+    code: "CODE10",
     slots: [6, 7],
   },
 ] as const;
 
-let exceptionalDiscount: ExceptionalDiscount | null = null;
+const exceptionalDiscount: ExceptionalDiscount | null = null;
 
-export const promotional = {
+export const promotional: {
+  totalSlots: number;
+  discountStates: typeof discountStates;
+  exceptionalDiscount: ExceptionalDiscount | null;
+} = {
   totalSlots,
   discountStates,
   exceptionalDiscount,
-};
+} as const;
