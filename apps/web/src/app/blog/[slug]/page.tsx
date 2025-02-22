b2eb2b70db9e5d1af2d174a72746727cadcde8fb
Anthony Cyrille <ancyr@pm.me>
2025-02-22 11:41:24 +0100

    task: page

--- apps/web/src/app/blog/[slug]/page.tsx
+++ apps/web/src/app/blog/[slug]/page.tsx
@@ -1,13 +1,11 @@
 import path from "node:path";
 import * as fs from "node:fs";
 import matter from "gray-matter";
-import { visit } from "unist-util-visit";
-import { h } from "hastscript";
 
 import { BlogPage } from "../../../components/BlogPage";
 import { markdownToHtml } from "../../../markdown";
 
-const Page = async ({ params }: { params: Awaited<{ slug: string }> }) => {
+const Page = async ({ params }: { params: any }) => {
   const { slug } = await params;
 
   const fullPath = path.resolve("src", "posts", slug + ".md");
@@ -32,26 +30,3 @@ export const generateStaticParams = () => {
 };
 
 export default Page;
-
-function customElementsPlugin() {
-  return function (tree: any) {
-    visit(tree, function (node) {
-      if (
-        node.type === "containerDirective" ||
-        node.type === "leafDirective" ||
-        node.type === "textDirective"
-      ) {
-        if (node.name !== "notice") return;
-
-        const data = node.data || (node.data = {});
-        const tagName = node.type === "textDirective" ? "span" : "div";
-
-        data.hName = tagName;
-        data.hProperties = h(tagName, {
-          ...node.attributes,
-          class: `notice ${node.attributes.class}`,
-        }).properties;
-      }
-    });
-  };
-}
