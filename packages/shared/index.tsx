fa7f2ef3aac3e1fb3b3131ad0d026b285000a1bc
ancyrweb <ancyr@pm.me>
2025-03-20 09:23:59 +0100

    task: renamed

--- packages/shared/index.tsx
+++ packages/shared/index.tsx
@@ -1,8 +1,8 @@
 export { useDiscountDuration, useCurrentDiscount } from "./src/discount-hooks";
 export { cleanArchitectureCourses } from "./src/content/clean-architecture";
 export { allCourses } from "./src/content/all-courses";
-export { allWorkshops } from "./src/content/trainings";
-export type { WorkshopType } from "./src/content/trainings";
+export { allWorkshops } from "./src/content/workshops.js";
+export type { WorkshopType } from "./src/content/workshops.js";
 export { promotional } from "./src/content/promotional";
 export { Duration } from "./src/lib/duration";
 
